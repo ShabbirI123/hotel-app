7d3dede0e53e060ef5631673c69b0fade39592bc
Shabbir <shabbirislam801@gmail,com>
2022-11-20 16:03:16 +0100

    added booking delete

--- src/app/create-booking/create-booking.component.ts
+++ src/app/create-booking/create-booking.component.ts
@@ -22,8 +22,10 @@ export class CreateBookingComponent implements OnInit {
     endDate: new Date()
   }
 
+  exist: boolean = (this.activatedRoute.snapshot.paramMap.get('id')) ? true : false;
+
   ngOnInit(): void {
-    if (this.router.url != 'create'){
+    if (this.router.url != '/create'){
       let id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
       let bookingById = Bookings.find(booking => booking.id == id)!;
       this.booking = bookingById;
@@ -31,7 +33,34 @@ export class CreateBookingComponent implements OnInit {
   }
 
   save(): void {
-    Bookings.push(this.booking);
+    let bookingById = Bookings.find(booking => booking.id == this.booking.id);
+
+    if (bookingById == null || bookingById == undefined){
+      Bookings.push(this.booking);
+    } else {
+      bookingById = this.booking;
+    }
     this.router.navigate(['bookings']);
+  }
+
+  delete(): void {
+    let bookingById = Bookings.find(booking => booking.id == this.booking.id);
+    if (bookingById){
+      let index = Bookings.indexOf(this.booking);
+      if (index !== -1){
+        Bookings.splice(index, 1);
+      }
+    }
+    this.router.navigate(['bookings']);
+  }
+
+  dateChanged(event: Event, isStart: boolean){
+    let val = (event.target as HTMLInputElement).value;
+
+    if (isStart){
+      this.booking.startDate = new Date(val);
+    } else {
+      this.booking.endDate = new Date(val);
+    }
   }
 }
